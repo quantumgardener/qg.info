b219ac84e502bce7f5997d7dace3b18a017f3beb
David Buchan <david@buchan.org>
2025-04-04 11:42:05 +1100

    Quartz update fixes

--- quartz/components/scripts/sitetoc.inline.ts
+++ quartz/components/scripts/sitetoc.inline.ts
@@ -1,3 +1,14 @@
+function toggleToc(this: HTMLElement) {
+  this.classList.toggle("collapsed")
+  this.setAttribute(
+    "aria-expanded",
+    this.getAttribute("aria-expanded") === "true" ? "false" : "true",
+  )
+  const content = this.nextElementSibling as HTMLElement | undefined
+  if (!content) return
+  content.classList.toggle("collapsed")
+}
+
 function setupToc() {
   for (const toc of document.getElementsByClassName("sitetoc")) {
     const button = toc.querySelector(".sitetoc-header")

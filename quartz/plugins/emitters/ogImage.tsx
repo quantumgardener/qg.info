f13d99cfca665c9614744e92c61620447e92df37
David Buchan <david@buchan.org>
2025-04-11 09:39:02 +1000

    Set correct social header image

--- quartz/plugins/emitters/ogImage.tsx
+++ quartz/plugins/emitters/ogImage.tsx
@@ -155,7 +155,7 @@ export const CustomOgImages: QuartzEmitterPlugin<Partial<SocialImageOptions>> = 
             const generatedOgImagePath = isRealFile
               ? `https://${baseUrl}/${pageData.slug!}-og-image.webp`
               : undefined
-            const defaultOgImagePath = `https://${baseUrl}/static/og-image.png`
+            const defaultOgImagePath = `https://${baseUrl}/static/qg-image-500.webp`
             const ogImagePath = userDefinedOgImagePath ?? generatedOgImagePath ?? defaultOgImagePath
             const ogImageMimeType = `image/${getFileExtension(ogImagePath) ?? "png"}`
             return (

664f0a7bed23c81603936ce93a492308be982461
David Buchan <david@buchan.org>
2025-05-16 17:04:15 +1000

    Filter out photo backlinks.

--- quartz/components/Backlinks.tsx
+++ quartz/components/Backlinks.tsx
@@ -24,7 +24,9 @@ export default ((opts?: Partial<BacklinksOptions>) => {
     cfg,
   }: QuartzComponentProps) => {
     const slug = simplifySlug(fileData.slug!)
-    const backlinkFiles = allFiles.filter((file) => file.links?.includes(slug))
+    const backlinkFiles = allFiles
+      .filter((file) => file.links?.includes(slug))
+      .filter(f => !f.slug?.startsWith("photos"));  // stop links to photos
     if (options.hideWhenEmpty && backlinkFiles.length == 0) {
       return null
     }

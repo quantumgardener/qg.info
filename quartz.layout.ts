9051b1091459211df093e4e3bdeed123c96f81b1
David Buchan <david@buchan.org>
2025-05-23 14:35:51 +1000

    Remove reader function

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -42,7 +42,6 @@ export const defaultContentPageLayout: PageLayout = {
           grow: true,
         },
         { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
       ],
     }),
     //Component.Explorer(),

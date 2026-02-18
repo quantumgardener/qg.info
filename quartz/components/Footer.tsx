1dcc051a27dfe0a3de2c4d0bb43270e55f45478f
quantumgardener <david@buchan.org>
2026-02-18 21:01:54 +1100

    Change ai-policy link

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -26,7 +26,7 @@ export default ((opts?: Options) => {
           <li><a href="/about"><i class="nf nf-fa-address_card"></i> About</a></li>
           <li>|</li>
           <li><a href="/privacy"><i class="nf nf-fa-lock"></i> Privacy</a></li>
-          <li><a href="/ai"><i class="nf nf-md-brain"></i> AI Policy</a></li>
+          <li><a href="/ai-policy"><i class="nf nf-md-brain"></i> AI Policy</a></li>
           <li>|</li>
           <li><a href="https://aus.social/@dcbuchan"><i class="nf nf-fa-mastodon"></i> Mastodon</a></li>
           <li><a href="https://github.com/quantumgardener"><i class="nf nf-fa-github"></i> Github</a></li>

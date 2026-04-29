377117032e9e5bfef90fea6666edf53016cd00c8
David Buchan <david@buchan.org>
2026-04-30 09:41:44 +1000

    Tighten check for missing publish property.

--- quartz/plugins/filters/portcullis.ts
+++ quartz/plugins/filters/portcullis.ts
@@ -1,3 +1,4 @@
+import { findMixin } from "pixi.js";
 import { PublishStatus } from "../../util/myUtils";
 import { QuartzFilterPlugin } from "../types"
 import chalk from "chalk";
@@ -14,45 +15,45 @@ export const Portcullis: QuartzFilterPlugin = () => ({
   name: "Portucllis",
   shouldPublish(_ctx, [_tree, vfile]) {
 
-    const publish = vfile.data?.frontmatter?.publish
-    if(publish == null) {
+    const frontmatter = vfile.data?.frontmatter
+    if(!frontmatter || frontmatter.publish === undefined) {
         //full abort, this is an error condition in the originating vault. Every file must have publish set
-        console.error(chalk.redBright(`Publish state missing in: `), chalk.yellow(`${vfile.data?.frontmatter?.title}\n`));
+        console.error(chalk.redBright(`Publish state missing in: `), chalk.yellow(`${frontmatter?.title}\n`));
         process.exit(1)
     }
 
-    switch(publish) {
+    switch(frontmatter.publish) {
         case PublishStatus.DENY:
-            console.debug(chalk.yellow(`Deny: ${vfile.data?.frontmatter?.title}`))
+            console.debug(chalk.yellow(`Deny: ${frontmatter?.title}`))
             return false;
         case PublishStatus.UNDECIDED:
-            console.debug(chalk.yellow(`Undecided: ${vfile.data?.frontmatter?.title}`))
+            console.debug(chalk.yellow(`Undecided: ${frontmatter?.title}`))
             return false
         case PublishStatus.ALLOW:
             // Check for illegal tags
-            const tags = vfile.data?.frontmatter?.tags ?? []
+            const tags = frontmatter?.tags ?? []
             const illegalTagsFound = tags.some(tag =>
                 illegalTagPatterns.some(prefix =>
                     tag.toLowerCase().startsWith(prefix)
                 )
             )
             if(illegalTagsFound) {
-                console.debug(chalk.yellow(`Illegal tags in: ${vfile.data?.frontmatter?.title}`))
+                console.debug(chalk.yellow(`Illegal tags in: ${frontmatter?.title}`))
                 return false
             }
 
             // A date in the future means do not publish yet
-            const noteDate = new Date(vfile.data?.frontmatter?.datetime as string)
+            const noteDate = new Date(frontmatter?.datetime as string)
             const now = new Date()
             if(noteDate > now) {
-                console.info(chalk.yellow(`Future dated: ${vfile.data?.frontmatter?.title}`))
+                console.info(chalk.yellow(`Future dated: ${frontmatter?.title}`))
                 return false
             }
-            //console.debug(chalk.greenBright(`Allow: ${vfile.data?.frontmatter?.title}`))
+            //console.debug(chalk.greenBright(`Allow: ${frontmatter?.title}`))
             return true
         default:
             // Error, unknown value
-            console.error(chalk.redBright(`Unrecognised publish state "${publish}" in: `), chalk.yellow(`${vfile.data?.frontmatter?.title}\n`));
+            console.error(chalk.redBright(`Unrecognised publish state "${frontmatter.publish}" in: `), chalk.yellow(`${frontmatter?.title}\n`));
             process.exit(1)
     }
   },
